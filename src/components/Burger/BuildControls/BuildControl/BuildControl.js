50acc267ea1cac83c8a1abc1b5fd1895d0871241
akmal2409 <akmalalikhujaev@gmail.com>
2020-11-22 21:17:36 +0100

    Added burger, and controls for amount of ingredients

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,49 @@
+import React from "react";
+import classes from './BuildControl.module.css';
+
+const buildControl = (props) => {
+  // const Div = Styled.div`
+  //   display: flex;
+  //   justify-content: space-between;
+  //   align-items: center;
+  //   margin: 5px 0;
+  // `;
+
+  // const Button = Styled.button`
+  //   display: block;
+  //   font: inherit;
+  //   padding: 5px;
+  //   margin: 0 5px;
+  //   width: 80px;
+  //   border: 1px solid #AA6817;
+  //   cursor: pointer;
+  //   outline: none;
+  //   &:disabled {
+  //     background-color: #AC9980;
+  //     border: 1px solid #7E7365;
+  //     color: #ccc;
+  //     cursor: default;
+  //   }
+  //   &:hover:disabled {
+  //     background-color: #AC9980;
+  //     color: #ccc;
+  //     cursor: not-allowed;
+  //   }
+  // `;
+
+  // const Label = Styled.div`
+  //   padding: 10px;
+  //   font-weight: bold;
+  //   width: 80px;
+  // `;
+
+  return (
+    <div className={classes.BuildControl}>
+      <div className={classes.Label}>{props.label}</div>
+      <button className={classes.Less}>Less</button>
+      <button className={classes.More}>More</button>
+    </div>
+  );
+};
+
+export default buildControl;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes, { number } from "prop-types";
+
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient type={igKey} key={index + igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+    if(ingredients.length === 0) {
+      ingredients = <p>Please start adding ingredients</p>;
+    }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {ingredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+burger.propTypes = {
+  ingredients: PropTypes.object,
+};
+
+export default burger;
