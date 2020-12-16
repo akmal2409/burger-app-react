14055f8137068dddddc4f7398b997f9ce4a4930b
akmal2409 <akmalalikhujaev@gmail.com>
2020-12-16 18:55:51 +0100

    Added redux to the project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+import reducer from "./store/reducer";
+import "./index.css";
+import App from "./App";
+
+const store = createStore(reducer);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-ReactDOM.render(
-  app,
-  document.getElementById('root')
-);
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from './actionTypes';
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+  }
+
+  return state;
+}
+
+export default reducer;
